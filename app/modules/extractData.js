3ab948b1133bd7740c29c329bf853f3652e34346
RyelBanfield <ryelbanfield@gmail.com>
2021-11-02 11:06:15 -0400

    Prepare widget from development

--- app/modules/extractData.js
+++ app/modules/extractData.js
@@ -24,55 +24,7 @@ export const extractData = (sheetType, submittedFile) => {
 
           const asyncSwitch = async (sheetType) => {
             switch (sheetType) {
-              case 'Customers-Salespersons':
-                console.log(sheet, formData.data);
-                postForm(sheet, formData);
-                break;
-              case 'Sales':
-                const records = {};
-
-                await ZOHO.CREATOR.API.getAllRecords({
-                  reportName: 'Customer_Report',
-                }).then((response) => {
-                  records['customerReport'] = response.data;
-                });
-
-                await ZOHO.CREATOR.API.getAllRecords({
-                  reportName: 'Salesperson_Report',
-                }).then((response) => {
-                  records['salespersonReport'] = response.data;
-                });
-
-                await ZOHO.CREATOR.API.getAllRecords({
-                  reportName: 'Product_Report',
-                }).then((response) => {
-                  records['productReport'] = response.data;
-                });
-
-                const { customerReport, salespersonReport, productReport } =
-                  records;
-
-                const customerID = customerReport.find(
-                  (customer) =>
-                    customer.Customer_Name.display_value ===
-                    formData.data.Customer
-                ).ID;
-                formData.data.Customer = customerID;
-
-                const salespersonID = salespersonReport.find(
-                  (salesperson) =>
-                    salesperson.Salesperson_Name.display_value ===
-                    formData.data.Salesperson
-                ).ID;
-                formData.data.Salesperson = salespersonID;
-
-                formData.data.Line_Items.forEach((lineItem) => {
-                  const productID = productReport.find(
-                    (product) =>
-                      product.Product_Name.display_value === lineItem.Product
-                  ).ID;
-                  lineItem.Product = productID;
-                });
+              case 'Companies':
                 console.log(sheet, formData.data);
                 postForm(sheet, formData);
                 break;
